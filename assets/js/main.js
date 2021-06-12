5312f469045637333706f665298822f9a0b753ce
Adharsh M <adharsh@adharsh.in>
2021-06-12 15:41:47 +0530

    tweaks in ui.

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,18 @@ const skillsContent = document.getElementsByClassName("skills__content"),
 function toggleSkills() {
 	let itemClass = this.parentNode.className;
 
-	for (i = 0; i < skillsContent.length; i++) {
-		skillsContent[i].className = "skills__content skills__close";
-	}
+	// for (i = 0; i < skillsContent.length; i++) {
+	// 	skillsContent[i].className = "skills__content skills__close";
+	// }
 
 	if (itemClass === "skills__content skills__close") {
 		this.parentNode.className = "skills__content skills__open";
 	}
+
+	if (itemClass === "skills__content skills__open") {
+		this.parentNode.className = "skills__content skills__close";
+	}
+
 }
 
 skillsHeader.forEach((el) => {
